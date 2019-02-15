9104f6ae9908374642110c2774da6c148a123de6
Rupa Dhillon <rupa211984@gmail.com>
2019-02-15 15:37:57 -0800

    Finish initialize & player create methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,26 @@
+require_relative './board.rb'
+require_relative './display.rb'
+require_relative './human_player.rb'
+
 class Game
+  def initialize(player_arr)
+    @board = Board.new
+    @display = Display.new(@board)
+    @players = create_players(player_arr)
+    @current_player = @players[:white]
+  end
+
+  private
+  
+  def create_players(player_arr)
+    p_black = choose_player_type(player_arr[0], :black)
+    p_white = choose_player_type(player_arr[1], :white)
+    { black: p_black, white: p_white }
+  end
+
+  def choose_player_type(str, color)
+    str == 'human' ? Player.new(color, @display) : ComputerPlayer.new(color, @display)
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
