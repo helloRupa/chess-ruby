b3b0c10e7e640e9484b72ff8cc89b5bf09ad7d17
Rupa Dhillon <rupa211984@gmail.com>
2019-02-15 16:38:25 -0800

    Add make_move

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,6 +1,4 @@
 require_relative './player.rb'
-require_relative './display.rb'
-require_relative './board.rb'
 
 class HumanPlayer < Player
   def initialize(color, display)
@@ -8,23 +6,43 @@ class HumanPlayer < Player
   end
 
   def make_move(_board)
-    p get_pos
+    start_pos = nil
+    end_pos = nil
+    while end_pos.nil?
+      start_pos = get_start_pos
+      end_pos = get_end_pos
+    end
+    @display.board.move_piece!(@color, start_pos, end_pos)
   end
 
   private
 
-  def get_pos
-    pos = []
-    until @display.cursor.selected
-      @display.render
-      pos = @display.cursor.get_input
-    end
-    @display.cursor.toggle_selected
+  def get_start_pos
+    pos = get_pos until @display.cursor.selected
     pos
+  end
+
+  def get_end_pos
+    pos = nil
+    while @display.cursor.selected
+      pos = get_pos
+      if pos == 'undo'
+        @display.cursor.toggle_selected
+        return nil
+      end
+    end
+    pos
+  end
+
+  def get_pos
+    @display.render
+    @display.cursor.get_input
   end
 end
 
 if $PROGRAM_NAME == __FILE__
+  require_relative './display.rb'
+  require_relative './board.rb'
   h = HumanPlayer.new(:white, Display.new(Board.new))
   h.make_move(nil)
 end
