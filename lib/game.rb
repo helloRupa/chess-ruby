4452d6a7c881d5eba5a62aeedafd0476f3e008c9
Rupa Dhillon <rupa211984@gmail.com>
2019-02-15 19:48:11 -0800

    Add play loop & supporting methods & msg methods

--- lib/game.rb
+++ lib/game.rb
@@ -8,9 +8,55 @@ class Game
     @display = Display.new(@board)
     @players = create_players(player_arr)
     @current_player = @players[:white]
+    welcome_msg
+    play
+  end
+
+  def play
+    loop do
+      in_check_msg
+      @display.render
+      @current_player.make_move(@board)
+      break if won?
+      swap_player
+    end
+    win_msg
   end
 
   private
+
+  def in_check_msg
+    color = @current_player.color
+    return unless @board.in_check?(color)
+    system('clear')
+    puts "#{color.upcase}, you are in check!"
+    sleep(2)
+  end
+
+  def won?
+    color = @current_player.color
+    opponent = @board.get_opponent(color)
+    @board.checkmate?(opponent)
+  end
+
+  def welcome_msg
+    system('clear')
+    puts 'Welcome to Chess!'
+    puts
+    puts 'Use the arrow keys to move around the board.'
+    puts 'Press Enter to select a piece, then select a space to move to and press Enter again.'
+    puts 'To undo a selection, press Escape before choosing a space to move to.'
+    sleep(5)
+  end
+
+  def win_msg
+    puts
+    puts "Congratulations #{@current_player.color}! You won!"
+  end
+
+  def swap_player
+    @current_player = @current_player == @players[:white] ? @players[:black] : @players[:white]
+  end
   
   def create_players(player_arr)
     p_black = choose_player_type(player_arr[0], :black)
@@ -19,9 +65,10 @@ class Game
   end
 
   def choose_player_type(str, color)
-    str == 'human' ? Player.new(color, @display) : ComputerPlayer.new(color, @display)
+    str.downcase == 'human' ? HumanPlayer.new(color, @display) : ComputerPlayer.new(color, @display)
   end
 end
 
 if $PROGRAM_NAME == __FILE__
+  game = Game.new(['human', 'human'])
 end
