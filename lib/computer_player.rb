240f611a8c4ab929edc545cf31936454b75b3bf3
Rupa Dhillon <rupa211984@gmail.com>
2019-02-18 16:40:09 -0800

    Make random moves

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -6,5 +6,27 @@ class ComputerPlayer < Player
   end
 
   def make_move(board)
+    piece = choose_random_piece(board)
+    move_cursor(piece.pos)
+    sleep(1)
+    end_pos = choose_end_pos(piece)
+    move_cursor(end_pos)
+    board.move_piece!(@color, piece.pos, end_pos)
+  end
+
+  private
+
+  def choose_random_piece(board)
+    pieces = board.pieces(@color).reject { |piece| piece.moves.empty? }
+    pieces.sample
+  end
+
+  def choose_end_pos(piece)
+    piece.moves.sample
+  end
+
+  def move_cursor(end_pos)
+    @display.cursor.cursor_pos = end_pos
+    @display.render(@color)
   end
 end
