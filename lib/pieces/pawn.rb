d05ef9bd25e7f88a2c8723410697be98695633ad
Rupa Dhillon <rupa211984@gmail.com>
2019-02-20 00:05:57 -0800

    Handle en passant

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,8 +1,12 @@
 require_relative './piece.rb'
 
 class Pawn < Piece
+  attr_reader :en_passant
+  
   def initialize(color, board, pos)
     super
+    @en_passant = false
+    @first_move = true
   end
 
   def symbol
@@ -13,6 +17,7 @@ class Pawn < Piece
     all_moves = []
     all_moves.concat(get_forward_moves)
     all_moves.concat(get_side_attacks)
+    all_moves.concat(get_en_passant_attacks)
     all_moves
   end
 
@@ -20,18 +25,74 @@ class Pawn < Piece
     at_end_row?
   end
 
+  def set_en_passant
+    return unless @first_move
+    @en_passant = true if two_step? && pawns_to_left_right?
+  end
+
+  def en_passant_false
+    @en_passant = false
+  end
+
+  def set_first_move
+    @first_move = false
+  end
+
+  def forward_dir
+    @color == :black ? 1 : -1
+  end
+
   private
+
+  def get_en_passant_attacks
+    y, x = @pos
+    all_moves = []
+
+    side_attacks.each do |delta_y, delta_x|
+      pos_to_test = [y + delta_y, x + delta_x]
+      next unless @board.valid_pos?(pos_to_test)
+      piece = @board[pos_to_test]
+      all_moves << pos_to_test if piece.empty? && enemy_pawn_behind?(pos_to_test)
+    end
+    all_moves
+  end
+
+  def enemy_pawn_behind?(pos)
+    pos_to_test = [pos[0] - forward_dir, pos[1]]
+    return false unless @board.valid_pos?(pos_to_test)
+    piece = @board[pos_to_test]
+    opponent?(piece) && piece.is_a?(Pawn) && piece.en_passant
+  end
+
+  def two_step?
+    @pos[0] == start_row + (forward_dir * 2)
+  end
+
+  def pawns_to_left_right?
+    left_right_pos.each do |coords|
+      next unless @board.valid_pos?(coords)
+      piece = @board[coords]
+      return true if opponent?(piece) && piece.is_a?(Pawn)
+    end
+    false
+  end
+
+  def left_right_pos
+    left = [@pos[0], @pos[1] - 1]
+    right = [@pos[0], @pos[1] + 1]
+    [left, right]
+  end
 
   def at_end_row?
     @color == :black ? (pos[0] == 7) : (pos[0] == 0)
   end
 
   def at_start_row?
-    @color == :black ? (pos[0] == 1) : (pos[0] == 6)
+    pos[0] == start_row
   end
 
-  def forward_dir
-    @color == :black ? 1 : -1
+  def start_row
+    @color == :black ? 1 : 6
   end
 
   def forward_steps
