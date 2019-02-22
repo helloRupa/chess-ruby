7d91a0614123d4bea72d8a6ce8cdaa55e89839c5
Rupa Dhillon <rupa211984@gmail.com>
2019-02-21 18:13:58 -0800

    Refactor choose_best_move

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -4,19 +4,27 @@ require_relative './point_system.rb'
 class ComputerPlayer < Player
   def initialize(color, display)
     super
+    @best_piece = nil
+    @best_move = nil
   end
 
   def make_move(board)
     # piece = choose_random_piece(board)
-    piece, end_pos = choose_best_move(board)
-    move_cursor(piece.pos)
+    choose_best_move(board)
+    move_cursor(@best_piece.pos)
     sleep(1)
     # end_pos = choose_end_pos(piece)
-    move_cursor(end_pos)
-    board.move_piece!(@color, piece.pos, end_pos)
+    move_cursor(@best_move)
+    board.move_piece!(@color, @best_piece.pos, @best_move)
+    reset_best_piece_move
   end
 
   private
+
+  def reset_best_piece_move
+    @best_piece = nil
+    @best_move = nil
+  end
 
   def choose_random_piece(board)
     pieces = board.pieces(@color).reject { |piece| piece.valid_moves.empty? }
@@ -24,27 +32,33 @@ class ComputerPlayer < Player
   end
 
   def choose_best_move(board)
-    best_piece = nil
-    best_move = nil
     highest_val = PointSystem.calc_board_val(@color, board.rows)
 
     board.pieces(@color).shuffle.each do |piece|
       all_moves = piece.valid_moves
       next if all_moves.empty?
-      best_piece, best_move = piece, all_moves[0] if best_piece.nil?
-      start_pos = piece.pos
-      all_moves.each do |move|
-        target_piece = board[move]
-        board.move_piece(piece, move)
-        board_val = PointSystem.calc_board_val(@color, board.rows)
-        board.undo_move_piece(start_pos, move, target_piece)
-        next unless board_val > highest_val
-        best_piece = piece
-        best_move = move
-        highest_val = board_val
-      end
+      @best_piece, @best_move = piece, all_moves[0] if @best_piece.nil?
+      test_all_moves(board, piece, all_moves, highest_val)
     end
-    [best_piece, best_move]
+  end
+
+  def test_all_moves(board, piece, all_moves, highest_val)
+    start_pos = piece.pos
+    all_moves.each do |move|
+      board_val = move_result(board, piece, start_pos, move)
+      next unless board_val > highest_val
+      @best_piece = piece
+      @best_move = move
+      highest_val = board_val
+    end
+  end
+
+  def move_result(board, piece, start_pos, move)
+    target_piece = board[move]
+    board.move_piece(piece, move)
+    board_val = PointSystem.calc_board_val(@color, board.rows)
+    board.undo_move_piece(start_pos, move, target_piece)
+    board_val
   end
 
   def choose_end_pos(piece)
